64dbb09ef5c37c8d24870dee913789f964ab2a2f
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-10-07 23:01:07 +0200

    value state

--- src/pages/Mint/index.jsx
+++ src/pages/Mint/index.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './mint.scss';
 import Footer from '../../components/Footer';
 
 const Mint = () => {
+  const [inputValue, setInputValue] = useState(1);
+
+  const updateInputValue = (e) => {
+    setInputValue(e.target.value)
+  }
 
   return (
     <div className="container__mint--all">
       <div className="container__btn--all">
         <input
-        className="first--element"
-        autoFocus
-        type="number"
-        max="10"
-        value="0" />
+          className="first--element"
+          autoFocus
+          type="number"
+          max="10"
+          onChange={updateInputValue}
+          value={inputValue}
+        />
         <button>
           Mint!
         </button>
