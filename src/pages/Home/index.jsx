104d7cb3b47b63aa47e3a73d8a01ea52954cc334
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-10-17 19:15:01 +0200

    HOME DOE

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import './home.scss';
 import { NavHashLink } from 'react-router-hash-link';
 import ArrowDown from '../../assets/SVG/ArrowDownLight';
+import Title from '../../assets/Logo/Title';
+import Subtitle from '../../assets/Logo/Subtitle';
 
 const Home = () => {
 
   return (
     <section className="container__section--home" id="home">
       <div className="container__content home--Section">
-        <div className="container--title--subtitle">
-          <h1 className="title">MINT<br/>YOUR<br/>MINT</h1>
-          <h3 className="subtitle">To mint mints<br/>is what we like to mint</h3>
-        </div>
+
+          <div className="container--title--subtitle">
+              <Title />
+              <Subtitle />
+            {/* <h1 className="title">MINT<br/>YOUR<br/>MINT</h1>
+            <h3 className="subtitle">To mint mints<br/>is what we like to mint</h3> */}
+          </div>
+
       </div>
       <div className="container__CTA--btn">
         <div className="CTA--btns">
--- src/pages/Roadmap/index.jsx
+++ src/pages/Roadmap/index.jsx
@@ -8,8 +8,13 @@ const Roadmap = () => {
 
   return (
     <section className="container__section--roadmap" id="roadmap">
-      <div className="container__content">
+      <div className="container__content roadmap">
         <h1 className="section--title">ROADMAP</h1>
+        <div className="container__description">
+          <p className="medium-size">
+            ¡COMING SOON!
+          </p>
+        </div>
       </div>
       <div className="container__CTA--btn">
         <div className="CTA--btns">
